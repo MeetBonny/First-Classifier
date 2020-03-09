a60a5c182cb09c54eb2c7b61fda40b520ccb4323
MeetBonny <43013550+MeetBonny@users.noreply.github.com>
2020-03-09 21:25:47 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/open?id=1XMt9oZLR7XdVkQqrMp9PczMsq3_cUgif'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1XMt9oZLR7XdVkQqrMp9PczMsq3_cUgif'
 export_file_name = 'export 2 .pkl'
 
 classes = ['Abyssinian',
